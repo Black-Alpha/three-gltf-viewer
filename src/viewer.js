b37cb1bffc942a01472af2791dd3f10da05f67d3
Black-Alpha <tragedy410@gmail.com>
2019-06-03 04:23:25 -0400

    Update viewer.js
    toon material

--- src/viewer.js
+++ src/viewer.js
@@ -191,6 +191,26 @@ module.exports = class Viewer {
       loader.load(url, (gltf) => {
 
         const scene = gltf.scene || gltf.scenes[0];
+        
+                scene.traverse(function(child) {
+                    if (child.isMesh) {
+                        // child.castShadow = true;
+                        // child.receiveShadow = false;
+                        // child.flatshading = true;
+                var material = new THREE.MeshToonMaterial({
+                  shininess: 0,
+                  skinning: true,
+                  color: child.material.color,
+                   side: THREE.DoubleSide
+                  // opacity: 0.1,
+                  // transparent: true,
+                  // depthWrite: false,
+                });
+                child.material = material;
+                    }
+                });
+        
+        
         const clips = gltf.animations || [];
         this.setContent(scene, clips);
 
